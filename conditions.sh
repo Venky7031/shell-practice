0a3035103e00359b7f77664451b6cc8de5f8ff9b
venky7031 <venkyvenky7031@gmail.com>
2025-07-02 23:24:59 +0530

    hollow world started

--- conditions.sh
+++ conditions.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u)  #This is the expression we have to give if any condions to write
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]  #Expression
 
 then
 
 echo "ERROR:: please run this script with root access"
 
-
+exit 1  #THis is if not root access it will print the above msg so we hv used
 else
 
 echo "your running with root access"
 
 fi
 
-dnf install mysql -y
+dnf install mysql -y #If you want to install this
 
-if [ $? eq 0 ]
+if [ $? eq 0 ] ##again write exp
 
 then
 
@@ -27,6 +27,8 @@ else
 
 echo "install mysql is .... failure"
 
-exit 1
+exit 1  #print the above msgs then we will get the output
 
 fi
+
+## after getting some error or output, if it is 0 then script runs if it is any number means whole script is failure
